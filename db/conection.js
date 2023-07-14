e50d6c4df89b4099435d487e4f47ef1533593e67
gyanlearning <139529488+gyanlearning@users.noreply.github.com>
2023-07-14 16:45:21 +0530

    Update conection.js

--- db/conection.js
+++ db/conection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect(process.env.DB_PATH,{
+mongoose.connect("mongodb+srv://Gaurav:gaurav9128@cluster0.zfgqj.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     family:4,
