eea3db6e65bd82d4c72bf26915ce0d62c01a54b7
NKBDVS23 <saurabhtyagi74@gmail.com>
2023-11-21 17:56:08 +0530

    modified

--- src/db/conection.js
+++ src/db/conection.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 const config=require("../config")
 
-mongoose.connect(config.DB_PATH,{
+mongoose.connect(`${config.DB_PATH}`,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     family:4,
